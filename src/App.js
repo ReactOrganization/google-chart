9fe03112311bbf82a6fdafe50f334b02e83955e6
americanman <snowboardracer7@gmail.com>
2018-10-15 21:43:29 -0400

    Chart is on the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
 
-class App extends Component {
+const pieOptions = {
+  title: "",
+  pieHole: 0,
+  slices: [
+    {
+      color: "#2BB673"
+    },
+    {
+      color: "#d91e48"
+    },
+    {
+      color: "#007fad"
+    },
+    {
+      color: "#e9a227"
+    }
+  ],
+  legend: {
+    position: "bottom",
+    alignment: "center",
+    textStyle: {
+      color: "233238",
+      fontSize: 14
+    }
+  },
+  tooltip: {
+    showColorCode: true
+  },
+  chartArea: {
+    left: 0,
+    top: 0,
+    width: "100%",
+    height: "80%"
+  },
+  fontName: "Roboto"
+};
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Chart
+          chartType="PieChart"
+          data={[["Age", "Weight"], ["Dog Lovers", 7 ], ["people who don't like dogs", 1]]}
+          options={pieOptions}
+          graph_id="PieChart"
+          width={"100%"}
+          height={"400px"}
+          legend_toggle
+        />
       </div>
     );
   }
