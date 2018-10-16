8add65daa46d6adf57d9c77f93c473f3e9b21dda
americanman <snowboardracer7@gmail.com>
2018-10-16 10:50:57 -0400

    Convert class component to functional/Pass props

--- src/App.js
+++ src/App.js
@@ -3,56 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 import { Chart } from "react-google-charts";
 
-const pieOptions = {
-  title: "",
-  pieHole: 0,
-  slices: [
-    {
-      color: "#2BB673"
+
+const Pie = (props) => {
+  const pieOptions = {
+    title: "",
+    pieHole: 0,
+    slices: [
+      {
+        color: "#2BB673"
+      },
+      {
+        color: "#d91e48"
+      },
+      {
+        color: "#007fad"
+      },
+      {
+        color: "#e9a227"
+      }
+    ],
+    legend: {
+      position: "bottom",
+      alignment: "center",
+      textStyle: {
+        color: "233238",
+        fontSize: 14
+      }
     },
-    {
-      color: "#d91e48"
+    tooltip: {
+      showColorCode: true
     },
-    {
-      color: "#007fad"
+    chartArea: {
+      left: 0,
+      top: 0,
+      width: "100%",
+      height: "80%"
     },
-    {
-      color: "#e9a227"
-    }
-  ],
-  legend: {
-    position: "bottom",
-    alignment: "center",
-    textStyle: {
-      color: "233238",
-      fontSize: 14
-    }
-  },
-  tooltip: {
-    showColorCode: true
-  },
-  chartArea: {
-    left: 0,
-    top: 0,
-    width: "100%",
-    height: "80%"
-  },
-  fontName: "Roboto"
-};
+    fontName: "Roboto"
+  };
+
+  return(
+    <Chart
+      chartType="PieChart"
+      data={[["Age", "Weight"], ["Dog Lovers", props.lovers ], ["people who don't like dogs", props.haters]]}
+      options={pieOptions}
+      graph_id="PieChart"
+      width={"100%"}
+      height={"400px"}
+      legend_toggle
+    />
+  )
+}
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Chart
-          chartType="PieChart"
-          data={[["Age", "Weight"], ["Dog Lovers", 7 ], ["people who don't like dogs", 1]]}
-          options={pieOptions}
-          graph_id="PieChart"
-          width={"100%"}
-          height={"400px"}
-          legend_toggle
-        />
+        <Pie lovers={9} haters={2}/>
       </div>
     );
   }
